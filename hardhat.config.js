5164f7caaf6b0636e8432e52a174d418b306b1a1
StyloIsRed <stylochime@gmail.com>
2024-08-03 02:09:53 +0100

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
-
-const PRIVATE_KEY = "";
 // Remember to use the private key of a testing account
 // For better security practices, it's recommended to use npm i dotenv for storing secret variables
+
+//use configuration-variables in hardhat to set PRIVATE_KEY variable
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 module.exports = {
   defaultNetwork: "swisstronik",
